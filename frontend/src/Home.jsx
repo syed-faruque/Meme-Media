510172201dbf389b4fe2cb1c648d1317e7e7b413
Syed Faruque <97230991+syed-faruque@users.noreply.github.com>
2024-03-15 10:31:35 -0400

    Update Home.jsx

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -7,9 +7,12 @@ axios.defaults.withCredentials = true;
 
 const Home = () => {
     const navigate = useNavigate();
+
+    //made a usestate here because I want the feeds variable to always be up to date
     const [feeds, setFeeds] = useState([]);
 
 
+    //function for fetching all the posts and their info from the server
     const getFeeds = () => {
         axios.get("http://192.168.0.202:1111/getfeeds")
         .then((response) => {
@@ -22,6 +25,7 @@ const Home = () => {
         getFeeds();
     }, []);
 
+    //function that lets the server know to update session variables and also naviagtes the user when he wants to view a post
     const handleViewPost = (index) => {
         axios.post("http://192.168.0.202:1111/viewpost", {user: feeds[index][0], file: feeds[index][1]})
         .then((response) => {
@@ -33,17 +37,18 @@ const Home = () => {
 
     };
 
+    //function that lets the server know if a user presses the like button. Allows for the liketable in the database to be updated. Also increments the value for feeds[index][3] which contains the amount of likes.
     const handleLike = (index) => {
         axios.post("http://192.168.0.202:1111/likepost", {user: feeds[index][0], file: feeds[index][1]})
         .then((response) => {
             if (response.data.valid){
                 const newfeeds = feeds.slice();
-                newfeeds[index][3] = newfeeds[index][3] + 1;
+                newfeeds[index][3] = newfeeds[index][3] + 1; //increments number of likes
                 setFeeds(newfeeds);
             }
             else{
                 const newfeeds = feeds.slice();
-                newfeeds[index][3] = newfeeds[index][3] - 1;
+                newfeeds[index][3] = newfeeds[index][3] - 1;    //decrements number of likes incase a user unlikes
                 setFeeds(newfeeds);
             }
         })
