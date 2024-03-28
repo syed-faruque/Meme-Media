7a1d4523c1a5cef184a6f13c063047314b9a4bb0
Syed Faruque <97230991+syed-faruque@users.noreply.github.com>
2024-03-28 17:28:44 -0400

    add comments

--- frontend/src/Success.jsx
+++ frontend/src/Success.jsx
@@ -1,5 +1,7 @@
+//necessary imports
 import { Link } from "react-router-dom";
 
+//displays a message to let the user know the signup was successful. Also provides a link back to the login page.
 const Success = () => {
     return(
         <div className="success_message">
