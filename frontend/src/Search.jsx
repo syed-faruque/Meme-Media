a78e40bdd5d656ea7544dcfa09931641afb7cb0f
Syed Faruque <97230991+syed-faruque@users.noreply.github.com>
2024-03-28 17:23:20 -0400

    add comments

--- frontend/src/Search.jsx
+++ frontend/src/Search.jsx
@@ -1,14 +1,18 @@
+//necessary imports
 import Navbar from "./Navbar";
 import axios from "axios";
 import {useState, useEffect} from "react";
 import { useNavigate } from "react-router-dom";
+
+//makes credentials visible for all requests sent out by axios
 axios.defaults.withCredentials=true;
 
+//search component
 const Search = () => {
-
     const [users, setUsers] = useState([]);
     const navigate = useNavigate();
 
+    //function for fetching searchresults from the server
     const fetchResults = () => {
         axios.get("http://192.168.0.202:1111/getsearchresults")
         .then((response)=>{
@@ -16,14 +20,17 @@ const Search = () => {
         })
     }
 
+    //function that navigates to the profile of the person the user clicks on
     const handleClick = (index) => {
         navigate(`/${users[index]}`);
     }
 
+    //initiates fetch function as soon as the component mounts.
     useEffect(()=>{
         fetchResults();
     }, [])
 
+    //maps out the search results.
     return(
         <div className="searchresults-container">
             <Navbar />
