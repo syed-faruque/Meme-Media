01c509e7712dc09f2ec7df246a48c96ad4c75684
Syed Faruque <talhafaruque6@gmail.com>
2024-04-17 22:42:28 -0400

    Update Home.jsx

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,18 +1,17 @@
-//necessary imports
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Navbar from "./Navbar";
 
-//makes credentials visible for all requests sent out by axios
+
 axios.defaults.withCredentials = true;
 
-//home component
+
 const Home = () => {
     const navigate = useNavigate();
     const [feeds, setFeeds] = useState([]);
 
-    //function for fetching feed data from the server.
+
     const getFeeds = () => {
         axios.get("http://192.168.0.202:1111/getfeeds")
         .then((response) => {
@@ -25,7 +24,7 @@ const Home = () => {
         getFeeds();
     }, []);
 
-    //function for informing the server of the post the user wants to view, and then navigating to the comment section of that post
+
     const handleViewPost = (index) => {
         axios.post("http://192.168.0.202:1111/viewpost", {user: feeds[index][0], id: feeds[index][5]})
         .then((response) => {
@@ -37,7 +36,7 @@ const Home = () => {
 
     };
 
-    //function for informing the server when the user presses the like button. This also increments the state of the like value so the user can see it change
+
     const handleLike = (index) => {
         axios.post("http://192.168.0.202:1111/likepost", {user: feeds[index][0], id: feeds[index][5]})
         .then((response) => {
@@ -54,7 +53,7 @@ const Home = () => {
         })
     };
 
-    //maps out all the post data that was fetched from the server
+ 
     return (
         <div className="home">
             <Navbar /><br></br><br></br>
