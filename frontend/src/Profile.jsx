c161a9992b543d217f6df57603e345ea45ac175b
Syed Faruque <talhafaruque6@gmail.com>
2024-04-17 22:44:46 -0400

    Update Profile.jsx

--- frontend/src/Profile.jsx
+++ frontend/src/Profile.jsx
@@ -1,19 +1,16 @@
-//necessary imports
 import Navbar from "./Navbar";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-//makes credentials visible for all requests sent out by axios
 axios.defaults.withCredentials = true;
 
-//profile component
 const Profile = () => {
     const navigate = useNavigate();
     const [username, setUsername] = useState('');
     const [userposts, setUserposts] = useState([]);
 
-    //function for fetching the user's data from the serverside
+
     const fetchData = () => {
         axios.get("http://192.168.0.202:1111/getuserdata")
             .then((response) => {
@@ -22,7 +19,6 @@ const Profile = () => {
             })
     }
 
-    //function for navigating to the post that the user clicks on
     const handleImageClick = (index) => {
         axios.post("http://192.168.0.202:1111/viewpost", {user: username, id: userposts[index][1]})
         .then((response) => {
@@ -33,13 +29,11 @@ const Profile = () => {
         .catch(error => console.error("Error fetching info:", error));
     }
 
-    //initiates the fetch functions as soon as the component mounts.
     useEffect(() => {
         fetchData();
     }, []);
 
 
-    //displays the username followed by a mapping of all the user's previous posts
     return (
         <div className="profile-container">
             <Navbar />
